627fc111253d2a5b110bb453c1946cd40d762956
binchen <chenbin_2010@sina.com>
2024-07-13 21:37:37 +0800

    update

--- src/Arr/ArrFormat.php
+++ src/Arr/ArrFormat.php
@@ -12,13 +12,13 @@ class ArrFormat
      * @param int $deep
      * @return array
      */
-    public static function arrayKeyToCamel(array $data, int $currentDeep = 1, int $deep = PHP_INT_MAX): array
+    public static function keyToCamel(array $data, int $currentDeep = 1, int $deep = PHP_INT_MAX): array
     {
         $arr = [];
         foreach ($data as $k => $v) {
             $key = is_numeric($k) ? $k : StrFormat::camel($k);
             if ($deep > $currentDeep && is_array($v)) {
-                $arr[$key] = self::arrayKeyToCamel($v, ++$currentDeep, $deep);
+                $arr[$key] = self::keyToCamel($v, ++$currentDeep, $deep);
             } else {
                 $arr[$key] = $v;
             }
